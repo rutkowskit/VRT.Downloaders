1795253efe9d92553376cfc3141ff1eeead309ad
rutkowskit <rutkowskit@gmail.com>
2021-06-24 09:22:44 +0200

    Refine Android Release configuration (for smaller apk size)

--- VRT.Downloaders.Mobile/VRT.Downloaders.Mobile.Android/Properties/AssemblyInfo.cs
+++ VRT.Downloaders.Mobile/VRT.Downloaders.Mobile.Android/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VRT.Downloaders.Mobile.Android")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VRT downloader")]
+[assembly: AssemblyDescription("VRT Media downloader for Android")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VRT.Downloaders.Mobile.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -22,9 +22,15 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.01.001")]
+[assembly: AssemblyFileVersion("1.01.001")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
