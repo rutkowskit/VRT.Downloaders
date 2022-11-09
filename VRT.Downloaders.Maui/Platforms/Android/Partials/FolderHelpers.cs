51cc126084c92cbb16161129b616863a5cb318c0
rutkowskit <rutkowskit@gmail.com>
2022-11-09 11:26:54 +0100

    Upgrade to .NET 7, remove wpf project, fix errors after migration

--- VRT.Downloaders.Maui/Platforms/Android/Partials/FolderHelpers.cs
+++ VRT.Downloaders.Maui/Platforms/Android/Partials/FolderHelpers.cs
@@ -1,54 +1,11 @@
-﻿using Android.Content;
-using Android.Provider;
-using AndroidX.DocumentFile.Provider;
-using CSharpFunctionalExtensions;
-using VRT.Downloaders.Maui.Platforms.Android.Partials;
+﻿using CSharpFunctionalExtensions;
 
 namespace VRT.Downloaders.Maui.Helpers;
 internal static partial class FolderHelpers
 {
-    private const int PickFolderRequestCode = 692348;
-
     public static partial async Task<Result<string>> PickFolder()
     {
-        var result = await Result.Try(() => PickFolderAsync());
-        return result;
-    }
-    static async Task<string> PickFolderAsync()
-    {
-        var action = Intent.ActionOpenDocumentTree;
-        var intent = new Intent(action);
-
-        var pickerIntent = Intent.CreateChooser(intent, "Select folder");
-
-        try
-        {
-            var result = string.Empty;
-            var x = await ProxyActivity.StartAsync(pickerIntent, PickFolderRequestCode, null, i => result = OnResult(i));
-            return result;
-        }
-        catch (OperationCanceledException)
-        {
-            return null;
-        }
-    }
-    private static string OnResult(Intent intent)
-    {
-        if (intent.Data is not null)
-        {
-            var path = EnsurePhysicalPath(intent.Data, true);
-            return path;
-        }
-        return null;
-    }
-    public static string EnsurePhysicalPath(Android.Net.Uri uri, bool requireExtendedAccess = true)
-    {
-#pragma warning disable CS0618 // Type or member is obsolete
-        var root = global::Android.OS.Environment.ExternalStorageDirectory.Path;
-#pragma warning restore CS0618 // Type or member is obsolete
-        var docId = DocumentsContract.GetTreeDocumentId(uri);
-
-        var docFile = DocumentFile.FromTreeUri(Android.App.Application.Context, uri);
-        return docFile.Uri.Path;
+        await Task.CompletedTask;
+        return Result.Failure<string>("Picking folder is not supported");        
     }
 }
